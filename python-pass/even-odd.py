ea1afb183c6bd696ea3e4fb132ddb578e5a38c17
Aya-Jafar <103113441+Aya-Jafar@users.noreply.github.com>
2022-04-06 17:49:43 +0300

    Create even-odd.py

---
+++ python-pass/even-odd.py
@@ -0,0 +1,16 @@
+
+def even_or_odd(num_of_elements):
+    even = []
+    odd= []
+    if num_of_elements > 0 and num_of_elements <= 10:
+        for i in range(num_of_elements):
+            n = int(input())
+            if n % 2 == 0:
+                even.append(n)
+            else:
+                odd.append(n)
+                
+        [print(j,"is even") for j in even]
+        [print(k,"is odd") for k in odd]
+
+even_or_odd(int(input("Enter X value: ")))
